fcb97f3411fdf5ee46157685ee280b00140c8216
Pokerznik-O <57524527+Pokerznik-O@users.noreply.github.com>
2019-11-26 12:08:34 +0100

    Update NewtonRaph.py

--- Oliver/NewtonRaph.py
+++ Oliver/NewtonRaph.py
@@ -1,1 +1,19 @@
 import math
+
+def function(x):
+    return math.exp(-x) - x
+
+
+def derivative(x):
+    return -(math.exp(-x)) - 1
+
+
+def roots_nr(guess, error):
+    ec = 100
+    while ec > error:
+        xn = guess - (function(guess)/derivative(guess))
+        ec = (guess - xn)/xn * 100
+        guess = xn
+    return xn
+
+roots_nr(1, 0)
